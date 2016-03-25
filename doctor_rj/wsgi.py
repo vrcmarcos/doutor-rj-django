6db9d15fb4e075f9a146241ab0ecc064f02a79bf
Daniel Daim <dandaim@gmail.com>
2016-03-25 16:31:21 -0300

    Teste Heroku

--- doctor_rj/wsgi.py
+++ doctor_rj/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.handlers.wsgi import WSGIHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doctor_rj.core.settings")
 
-application = WSGIHandler()
+application = get_wsgi_application()
