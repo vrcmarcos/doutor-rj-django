cf7437ef8b225cf50464d863492faa0770548f06
Daniel Daim <dandaim@gmail.com>
2016-03-25 16:37:57 -0300

    Teste Heroku

--- doctor_rj/wsgi.py
+++ doctor_rj/wsgi.py
@@ -11,12 +11,13 @@ import os
 import sys
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+print PROJECT_ROOT
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "doctor_rj"))
 sys.path.insert(1, os.path.join(PROJECT_ROOT, "doctor_rj/core"))
 sys.path.insert(2, os.path.join(PROJECT_ROOT, "doctor_rj/doctor_rj"))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doctor_rj.core.settings")
 
 application = get_wsgi_application()
